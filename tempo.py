3d947089016dc923ff6d6f1682da867af3d957f1
jrmylee <jeremy@capeanalytics.com>
2020-12-30 13:50:28 -0800

    fixed dependencies and renamed app

--- tempo.py
+++ tempo.py
@@ -1,12 +1,5 @@
 import numpy as np
 import librosa
-import matplotlib.pyplot as plt
-import pandas as pd
-import os
-import math
-import statistics
-from multiprocessing import Pool, cpu_count
-import numpy as np
 
 class Tempo:
     def __init__(self):
@@ -15,5 +8,4 @@ class Tempo:
     def global_tempo(self, signal, sr):
         onset_env = librosa.onset.onset_strength(y=signal, sr=sr)
         tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
-        print(tempo)
         return tempo[0]
